11213e251598d2d744aa0a78554a8557b77f4753
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-05-03 21:15:26 -0500

    -Test commit.

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -18,11 +18,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func didPressButton(_ sender: Any) {
         if(self.nameText.text != "") {
             helloLabel.text = "Hello \(nameText.text!)! Welcome to this app."
