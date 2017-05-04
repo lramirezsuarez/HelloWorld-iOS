83c3120321e509b8859bae0ddaf09b67e8e28cca
lramirezsuarez <lramirezsuarez@gmail.com>
2017-05-03 21:08:32 -0500

    First changes and some code to begin.

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Lucho on 5/2/17.
+//  Copyright © 2017 Lucho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var changeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPressButton(_ sender: Any) {
+        if(self.nameText.text != "") {
+            helloLabel.text = "Hello \(nameText.text!)! Welcome to this app."
+            nameText.text = ""
+        } else {
+            displayMessage(message: "Write something in the text field.")
+        }
+    }
+    
+    func displayMessage(message: String) {
+        let alert = UIAlertController(title: "Alert!", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
+
